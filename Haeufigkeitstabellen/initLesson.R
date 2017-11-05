753acb27cdfef08a1a8cf4b0e6873ea5c8d1f161
Stephan Weibelzahl <stephan.weibelzahl@gmail.com>
2017-11-05 19:07:13 +0100

    switch from freign to haven
    avoid import warnings for sav files in foreign. Correct typos. Add
    various questions. Prepare for public release.

--- Haeufigkeitstabellen/initLesson.R
+++ Haeufigkeitstabellen/initLesson.R
@@ -6,21 +6,15 @@ options(swirl_language = "german_formal")
 # SPSS-Daten von einer URL einlesen - Warnung kann ignoriert werden. Die Daten werden als Daten-Frame gespeichert.
 options(warn=-1)
 if(file.exists(file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Haeufigkeitstabellen", "06_Gesundheit.sav"))){
-  daten <- read.spss(
-    file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Haeufigkeitstabellen", "06_Gesundheit.sav"),
-    to.data.frame = TRUE,
-    max.value.labels = Inf, 
-    trim.factor.names = FALSE)
+  daten <- read_sav(file =
+    file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Haeufigkeitstabellen", "06_Gesundheit.sav"))
 }else if(!is.integer(try(download.file("http://www.google.de", tempfile(), quiet = TRUE), silent = TRUE))){
   stop("Dieses Tutorial benötigt eine funktionierende Internet-Verbindung. Bitte überprüfen Sie Ihre Verbindung!")
 }else{
-  daten <- read.spss(
-    "https://www3.hogrefe.de/fileadmin/redakteure/hogrefe_de/Psychlehrbuchplus/Multivariate_Verfahren/06_Analyse_mehrdim_Haeufigkeitst/Gesundheit.sav",
-    to.data.frame = TRUE,
-    max.value.labels = Inf, 
-    trim.factor.names = FALSE)
+  daten <- read_sav(
+    "https://www3.hogrefe.de/fileadmin/redakteure/hogrefe_de/Psychlehrbuchplus/Multivariate_Verfahren/06_Analyse_mehrdim_Haeufigkeitst/Gesundheit.sav")
 }
 # Den Objekten des Datensatzes als Namen die Variablen-Labels der SPSS Daten geben
-names(daten) <- attributes(daten)$variable.labels  
+# names(daten) <- attributes(daten)$variable.labels  
 #options(warn=0)
   
--- Zeitreihenanalyse/initLesson.R
+++ Zeitreihenanalyse/initLesson.R
@@ -8,24 +8,16 @@ rm(list = ls())
 # SPSS-Daten von einer URL einlesen - Warnung kann ignoriert werden. Die Daten werden als Daten-Frame gespeichert.
 options(warn = -1)
 if(file.exists(file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Zeitreihenanalyse", "07_Schach.sav"))){
-  daten <- read.spss(
-    file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Zeitreihenanalyse", "07_Schach.sav"),
-    to.data.frame = TRUE,
-    max.value.labels = Inf, 
-    trim.factor.names = FALSE)
+  daten <- read_sav(file =
+    file.path(path.package("swirl"), "Courses","Multivariate_Verfahren", "Zeitreihenanalyse", "07_Schach.sav"))
   daten <- daten$hf
 }else if(!is.integer(try(download.file("http://www.google.de", tempfile(), quiet = TRUE), silent = TRUE))){
   stop("Dieses Tutorial benötigt eine funktionierende Internet-Verbindung. Bitte überprüfen Sie Ihre Verbindung!")
 }else{
-  # , encoding = "UTF-8"))
-  #, encoding = "native.enc"
-  daten <- read.spss(
-    "https://www3.hogrefe.de/fileadmin/redakteure/hogrefe_de/Psychlehrbuchplus/Multivariate_Verfahren/07_Zeitreihenanalyse/Schach.sav",
-    to.data.frame = TRUE,
-    max.value.labels = Inf, 
-    trim.factor.names = FALSE) 
+  daten <- read_sav(file =
+    "https://www3.hogrefe.de/fileadmin/redakteure/hogrefe_de/Psychlehrbuchplus/Multivariate_Verfahren/07_Zeitreihenanalyse/Schach.sav") 
   daten <- daten$hf
 }  
 # Den Objekten des Datensatzes als Namen die Variablen-Labels der SPSS Daten geben
-names(daten) <- attributes(daten)$variable.labels  
+# names(daten) <- attributes(daten)$variable.labels  
 #options(warn=0)
